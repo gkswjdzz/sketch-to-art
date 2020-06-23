6f3a6c68b8836f8c42fa17986e011496fd2f30fc
Hanjeong Lee <gkswjdzz@naver.com>
2020-06-23 20:53:32 +0900

    fix canvas width, height, support touch for mobile

--- src/components/DrawingBoard.vue
+++ src/components/DrawingBoard.vue
@@ -5,11 +5,9 @@
           v-on:mouseup="stopPainting"
           v-on:mousemove="onMouseMove"
           v-on:mouseleave="stopPainting"
-          v-on:click="handleCanvasClick"
-          v-on:contextmenu="handleContextMenu"
-          v-on:
+
           v-on:touchstart="handleStart"
-          v-on:touchend="handleEnd"
+          v-on:touchend="stopPainting"
           v-on:touchcancel="handleCancel"
           v-on:touchmove="handleMove"
           :width="this.width"
@@ -24,11 +22,13 @@ export default {
   data: function() {
     return {
       INITIAL_COLOR: "#2c2c2c",
-      CANVAS_WIDTH: 800,
-      CANVAS_HEIGHT: 560,
+      CANVAS_WIDTH: '',
+      CANVAS_HEIGHT: '',
 
       painting: false,
-      filling: false
+      filling: false,
+      vw: 0,
+      vh: 0
       // mouse: {
       //   current: {
       //     x: 0,
@@ -60,10 +60,38 @@ export default {
   // },
 
   methods: {
+    handleStart: function(evt) {
+      console.log('start');
+      this.painting = true;
+      this.ctx.beginPath();
+      this.ctx.moveTo(x, y);
+    },
+    handleEnd: function() {
+
+    },
+    handleCancel: function() {
+      
+    },
+    handleMove: function(e) {
+      e.preventDefault();
+      const rect = e.target.getBoundingClientRect();
+      const x = e.touches[0].pageX - e.touches[0].target.offsetLeft;
+      const y = e.touches[0].pageY - e.touches[0].target.offsetTop;
+      
+      if (!this.painting) {
+        this.ctx.beginPath();
+        this.ctx.moveTo(x, y);
+      } else {
+        this.ctx.lineTo(x, y);
+        this.ctx.stroke();
+      }
+    },
     stopPainting: function() {
+      console.log('stop');
       this.painting = false;
     },
     startPainting: function() {
+      console.log('start');
       this.painting = true;
     },
     onMouseMove: function(event) {
@@ -78,105 +106,6 @@ export default {
         this.ctx.stroke();
       }
     },
-    handleColorClick: function(event) {
-      const color = event.target.style.backgroundColor;
-      this.ctx.strokeStyle = color;
-      this.ctx.fillStyle = color;
-    },
-    handleRangeChange: function(event) {
-      this.ctx.lineWidth = parseInt(event.target.value);
-    },
-    handleModeClick: function() {
-      if(this.filling === true) {
-        this.filling = false;
-        
-      } else {
-        filling = true;
-        
-      }
-    },
-    handleCanvasClick: function(){
-      if(this.filling === true) {
-        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-      }
-    },
-    handleContextMenu: function() {
-      event.preventDefault();
-    },
-    handleSaveClick: function() {
-      const image = canvas.toDataURL("image/png");
-      const link = document.createElement("a");
-      link.href = image;
-      link.download = "PaintJS[🎨]";
-      link.click();
-    },
-    draw: function(event) {
-      if (this.mouse.down && this.enabled) {
-        var c = document.getElementById("canvas");
-        var ctx = c.getContext("2d");
-        ctx.clearRect(0, 0, this.width, this.height);
-        ctx.lineCap = "round";
-        ctx.lineTo(this.currentMouse.x, this.currentMouse.y);
-        ctx.strokeStyle = this.lineColor;
-        ctx.lineWidth = this.lineWidth;
-        ctx.stroke();
-      }
-    },
-
-    handleMouseDown: function(event) {
-      console.log(event)
-      this.mouse.down = true;
-      this.mouse.current = {
-        x: event.clientX,
-        y: event.clientY
-      };
-
-      var c = document.getElementById("canvas");
-      var ctx = c.getContext("2d");
-
-      ctx.moveTo(this.currentMouse.x, this.currentMouse.y);
-    },
-    handleMouseUp: function() {
-      this.mouse.down = false;
-    },
-    handleMouseMove: function(event) {
-      this.mouse.current = {
-        x: event.pageX,
-        y: event.pageY
-      };
-
-      this.draw(event);
-    },
-
-    handleTouchStart: function(event) {
-      event.preventDefault();
-
-      var c = document.getElementById("canvas");
-      var ctx = c.getContext("2d");
-      var touch = event.touches[0];
-
-      this.mouse.down = true;
-      this.mouse.current = {
-        x: touch.clientX,
-        y: touch.clientY
-      };
-      ctx.moveTo(this.currentMouse.x, this.currentMouse.y);
-    },
-    handleTouchMove: function(event) {
-      event.preventDefault();
-      var touch = event.touches[0];
-      this.mouse.current = {
-        x: touch.eventX,
-        y: touch.eventY
-      };
-
-      this.draw(event);
-    },
-    handleTouchEnd: function(event) {
-      event.preventDefault();
-      this.mouse.down = false;
-    },
-
     clearCanvas: function() {
       var c = document.getElementById("canvas");
       c.width = c.width;
@@ -191,11 +120,27 @@ export default {
   },
 
   mounted: function() {
-    this.canvas= document.getElementById("canvas"),
-    this.ctx= this.canvas.getContext("2d"),
+    this.canvas = document.getElementById("canvas"),
+    this.ctx = this.canvas.getContext("2d"),
          
+    this.vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+    this.vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+    
+    if (this.vw > 900) {
+      this.CANVAS_WIDTH = 800;
+      this.CANVAS_HEIGHT = 560;
+    } else if (this.vw > 400) {
+      this.CANVAS_WIDTH = 520;
+      this.CANVAS_HEIGHT = 364;
+    } else {
+      this.CANVAS_WIDTH = 312;
+      this.CANVAS_HEIGHT = 236;
+    }
+
     this.canvas.width= this.CANVAS_WIDTH,
     this.canvas.height= this.CANVAS_HEIGHT,
+
+    console.log(this.canvas.width, this.canvas.height);
 
     this.ctx.fillStyle = "white";
     this.ctx.fillRect(0, 0, this.CANVAS_WIDTH, this.CANVAS_HEIGHT);
